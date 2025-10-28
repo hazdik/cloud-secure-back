97267e34c808fbb0bdb3428bb91b109918120d87
JayaseelanAG <jayaseelan.amithignanasekaran@programming.com>
2025-10-28 13:54:06 +0530

    kick 2

---
+++ webpack.queue.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './shared/services/MessagingService.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'queue.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+};
